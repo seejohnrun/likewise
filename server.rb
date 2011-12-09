08cbbab216edbbd02722ceb956ac2b44e2d9e095
John Crepezzi <john.crepezzi@gmail.com>
2011-12-09 17:18:30 -0500

    Expand API

--- server.rb
+++ server.rb
@@ -8,20 +8,30 @@ Likewise::store = Likewise::Store::Memcache.new
 get '/:type/:value/:relation' do
   headers['content-type'] = 'application/json'
   key = "#{params[:type]}-#{params[:value]}-#{params[:relation]}"
+  time = Time.now
   id = Digest::MD5.hexdigest(key)
   set = Likewise::SortedSet.find(id)
   # Return the set
   if set
     nodes = set.first(10)
     {
+      :time => Time.now - time,
       :length => set.length,
+      :total_weight => set.total_weight,
       :top => nodes.map { |node| {
         :name => node[:name],
         :weight => node.link[:weight],
-        :pct => node.link[:weight] / set.total_weight.to_f
+        :total => node[:weight],
+        :pct_of_set => node.link[:weight] / set.total_weight.to_f,
+        :pct_of_item => node.link[:weight] / node[:weight].to_f
       } }
     }.to_json
   else
-    { :length => 0, :total_weight => 0, :top => [] }.to_json
+    {
+      :time => Time.now - time,
+      :length => 0,
+      :total_weight => 0,
+      :top => []
+    }.to_json
   end
 end
