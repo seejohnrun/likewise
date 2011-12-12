3bc455bc7e447696fb77aa561a36570ab948d8d9
John Crepezzi <john.crepezzi@gmail.com>
2011-12-12 11:57:55 -0500

    Clean up some node code

--- lib/likewise/node.rb
+++ lib/likewise/node.rb
@@ -11,6 +11,9 @@ module Likewise
         find(data[:id]) || create(data)
       end
 
+      # Find Nodes
+      # @param [Array, String] ids - The id or ids to look up
+      # @return [Array, Likewise::Node] nodes - The nodes returned.  Array is ids is Array
       def find(ids)
         if ids.is_a?(Array)
           datum = Likewise::multiget(ids)
@@ -29,6 +32,9 @@ module Likewise
         end
       end
 
+      # Create a new node
+      # @param [Hash] data - Metadata for this node (optional)
+      # @return [Likewise::Node] the created node
       def create(data = {})
         node = new(data)
         node.instance_variable_set(:@id, data.delete(:id)) if data[:id]
@@ -37,17 +43,24 @@ module Likewise
 
     end
 
+    # The ID of this node
     attr_reader :id
+
+    # The link for the given context of this node
     attr_accessor :link
 
+    # Equality is based on node IDs
     def ==(node)
       node.id == id
     end
 
+    # Hash implemented for equality operation is based on IDs
     def hash
       node.id.hash
     end
    
+    # Set an attribute on this node
+    # TODO change out for #meta
     def []=(key, value)
       unless @data[key] == value
         @data[key] = value
@@ -55,14 +68,24 @@ module Likewise
       end
     end
 
+    # Get an attribute on this node
+    # TODO change out for #meta
     def [](key)
       @data[key]
     end
 
+    # Whether or not this node is currently persisted
+    # The way that nodes persist, whenever they have data changed,
+    # they will automatically be persisted, so this should only be false
+    # when using #new
+    # @return [Boolean] whether or not the node is persisted currently
     def persisted?
       !!@persisted
     end
 
+    # Save the given node to the datastore - giving it a new UUID as its
+    # id if it doesn't have one already.
+    # @return self
     def save
       @id ||= UUID.new.generate
       Likewise::set(@id, @data)
@@ -70,12 +93,14 @@ module Likewise
       self
     end
 
+    # Initialize a new Node with the given data set
     def initialize(data = {})
       @data = data
     end
 
     private
  
+    # Mark this node as persisted
     def persisted!
       @persisted = true
     end
